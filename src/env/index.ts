293b7c931cffcbab2abc87d8f68d04021bee48f0
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2024-09-26 12:21:59 -0300

    Add dotenv configuration for test environment

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,11 @@
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   PORT: z.coerce.number(),
