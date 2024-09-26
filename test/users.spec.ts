6921590bbc951b0cceeba1da6c594d6493029515
nicolasmacedoo <nicolas.conceicao01@gmail.com>
2024-09-26 12:49:33 -0300

    Add test for creating a user

---
+++ test/users.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it, beforeAll, afterAll, beforeEach } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+
+describe('Users routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a user', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+      })
+      .expect(201)
+  })
+})
